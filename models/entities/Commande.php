56bce266477227044a7db97a8e543842a5db5fca
yoann53 <yoann.gauchard@gmail.com>
2017-01-30 17:19:54 +0100

    new statut and commande class

---
+++ models/entities/Commande.php
@@ -0,0 +1,62 @@
+<?php
+
+class Commande
+{
+
+	private $id;
+	private $ref;
+	private $dateCommande;
+	private $dateExpedition;
+	private $client;
+	private $statut;
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setid($id) {
+		$this->id = $id;
+	}
+
+	public function getRef() {
+		return $this->ref;
+	}
+
+	public function setRef($ref) {
+		$this->ref = $ref;
+	}
+
+	public function getDateCommande() {
+		return $this->dateCommande;
+	}
+
+	public function setDateCommande($dateCommande) {
+		$this->dateCommande = $dateCommande;
+	}
+
+	public function getDateExpedition() {
+		return $this->dateExpedition;
+	}
+
+	public function setDateExpedition($dateExpedition) {
+		$this->dateExpedition = $dateExpedition;
+	}
+
+	public function getClient() {
+		return $this->client;
+	}
+
+	public function setClient($client) {
+		$this->client = $client;
+	}
+
+	public function getStatut() {
+		return $this->statut;
+	}
+
+	public function setStatut($statut) {
+		$this->statut = $statut;
+	}
+
+
+}
